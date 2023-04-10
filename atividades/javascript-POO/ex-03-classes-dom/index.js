29d10778f7460d3b061e6e9ee11896cf40c3ef01
João Paulo Moreira <jpmoreira195@outlook.com>
2023-04-10 17:55:56 -0300

    Ex 03 classes  no dom

--- atividades/javascript-POO/ex-03-classes-dom/index.js
+++ atividades/javascript-POO/ex-03-classes-dom/index.js
@@ -1,7 +1,28 @@
-import { Components } from "./Components.js";
+import { Component } from "./Component.js";
 import { Form } from "./Form.js";
 import { Input } from "./Input.js";
 import { Label } from "./Label.js";
 
-const title = new Components('h1', 'body',{ textContent : 'Olá, Mundo!' })
+const title = new Component('h1', 'body',{ textContent : 'Olá, Mundo!' })
 console.log(title)
+
+title.render()
+
+title.tag = 'h3'
+
+title.build().render()
+
+const form = new Form('body')
+
+const label = new Label('Nome:', form, {htmlFor: 'nameInput'})
+const input = new Input(form, {id: 'nameInput', name: 'name'})
+
+form.render()
+label.render()
+form.addChildren(input)
+form.addChildren(
+    new Component('br'),
+    new Component('br'),
+    new Label('Data de nascimento', {htmlForm: 'dayInput'}),
+    new Input(form, {id: 'birthdayInput', name: 'data', type: 'date'})
+)
